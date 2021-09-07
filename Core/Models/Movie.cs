67e3f4a0ed2614673f64ec8f5c6ed0f1646b3300
Leriatod <artem.viznuk@gmail.com>
2021-09-07 19:30:39 +0300

    Added Name column to movies table

--- Core/Models/Movie.cs
+++ Core/Models/Movie.cs
@@ -5,6 +5,9 @@ namespace MovieRentalsAPI.Core.Models
     public class Movie
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
         public int NumberInStock { get; set; }
         public decimal DailyRentalRate { get; set; }
         [Required]
---
+++ Migrations/20210907162922_AddedNameColumnToMoviesTable.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieRentalsAPI.Migrations
+{
+    public partial class AddedNameColumnToMoviesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Movies",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Movies");
+        }
+    }
+}
